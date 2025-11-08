2f252c105dcf96ec5d3b56e9a2fcc536a8872df6
Olga P. <zjkz14@gmail.com>
2025-11-08 22:20:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const contents = document.querySelectorAll('.program-line__content')
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr')
+
+    title.addEventListener('click', () => {
+        contents.forEach((item) => {
+            if (item !== elem) {
+                item.querySelector('.program-line__descr').classList.remove('active')
+            }
+        })
+
+        descr.classList.toggle('active')
+    })
+})
